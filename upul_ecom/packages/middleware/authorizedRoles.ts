16cfc97a56d26e77d93c7332b88f95b24a85b3fd
SaminuT <saminuhansaja.w@gmail.com>
2026-03-20 09:16:18 +0530

    user-auth-roles added

---
+++ upul_ecom/packages/middleware/authorizedRoles.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Response } from "express";
+import { AuthError } from "../error-handler";
+
+export const isAdmin = (req: any, res: Response, next: NextFunction) => {
+    if(req.role !== "admin") {
+        return next(new AuthError("Access denied: Admin only"));
+    }
+    next();
+};
+
+export const isUser = (req: any, res: Response, next: NextFunction) => {
+    if(req.role !== "user") {
+        return next(new AuthError("Access denied: User only"));
+    }
+    next();
+};
