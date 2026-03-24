578920a27ef23011121b8a83d3fe4e95adf54e8d
savithuv <savithuviran@gmail.com>
2026-03-24 07:44:47 +0530

    coupen service added

--- upul_ecom/apps/api/src/routes/main.router.ts
+++ upul_ecom/apps/api/src/routes/main.router.ts
@@ -8,6 +8,8 @@ import colorRoutes from '../color-service/color.routes';
 import {authRouter} from '../auth-service/routes/auth.router';
 import sizeChartRoutes from '../size-types-service/sizetypes.routes';
 
+import couponsRoutes from '../coupen-service/coupen.routes';
+
 const router: Router = express.Router();
 
 router.use('/auth', authRouter);
@@ -18,5 +20,7 @@ router.use('/imagekit', imagekitRoutes);
 router.use('/size-types', sizeTypeRoutes);
 router.use('/colors', colorRoutes);
 router.use('/size-charts', sizeChartRoutes);
+
+router.use('/coupons', couponsRoutes);
 
 export default router;
