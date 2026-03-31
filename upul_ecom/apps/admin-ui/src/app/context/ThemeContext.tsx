a7d58a686d0b7a97825c24f838c7633f28cadcb5
IsiraPabasara <155605271+IsiraPabasara@users.noreply.github.com>
2026-03-31 11:19:02 +0530

    Layout Fixed

---
+++ upul_ecom/apps/admin-ui/src/app/context/ThemeContext.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext({ isDark: false, toggleTheme: () => {} });
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isDark, setIsDark] = useState(false);
+
+  // Load saved theme on mount
+  useEffect(() => {
+    const saved = localStorage.getItem("theme");
+    if (saved === "dark") {
+      setIsDark(true);
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    setIsDark((prev) => {
+      const newTheme = !prev;
+      if (newTheme) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
+      return newTheme;
+    });
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
---
+++ upul_ecom/apps/admin-ui/src/app/hooks/useAdmin.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../axiosInstance";
+
+
+// fetch user data from API
+const fetchAdmin = async() => {
+    const response = await axiosInstance.get("/api/auth/logged-in-admin");
+
+    return response.data.user;
+}
+
+const useAdmin = () => {
+    const {data:user, isLoading, isError, refetch} = useQuery({
+        queryKey: ["admin"],
+        queryFn: fetchAdmin,
+        staleTime: 1000 * 60 * 5,
+        retry: 1,
+    })
+
+    return {user, isLoading, isError, refetch};
+}
+
+export default useAdmin;
