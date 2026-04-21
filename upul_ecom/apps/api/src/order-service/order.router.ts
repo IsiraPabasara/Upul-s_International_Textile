7193cc41385561fed20c86b983e881a528ef9fd3
ChathuniSihara <chathunisihara@gmail.com>
2026-04-21 22:01:02 +0530

    Update order.router.ts

--- upul_ecom/apps/api/src/order-service/order.router.ts
+++ upul_ecom/apps/api/src/order-service/order.router.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { cancelGuestOrder, cancelUserOrder, createOrder, getGuestOrder, getOrderById, getUserOrders } from './order.controller';
+import { cancelGuestOrder, cancelUserOrder, createOrder, getGuestOrder, getOrderById, getUserOrders, verifyOrder } from './order.controller';
 import isAuthenticated from '../../../../packages/middleware/isAuthenticated';
-import { getAllOrders, getOrderDetails, updateOrderStatus , getAdminOrderStats } from './admin.order.controller';
+import { isAdmin } from '../../../../packages/middleware/authorizedRoles';
+import { getAllOrders, getOrderDetails, updateOrderStatus , getAdminOrderStats , generateOrderReport } from './admin.order.controller';
 import { refundOrder } from './admin.refund.controller';
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 router.post('/', createOrder);
 router.get('/track/:token', getGuestOrder);
 router.patch('/track/:token/cancel', cancelGuestOrder);
+router.get('/verify/:orderNumber', verifyOrder);
 
 router.use(isAuthenticated);
 
@@ -16,10 +18,15 @@ router.get('/my-orders', getUserOrders);
 router.get('/my-orders/:id', getOrderById);
 router.patch('/my-orders/:id/cancel', cancelUserOrder);
 
+router.use(isAdmin);
 
 router.post('/admin/:orderId/refund', refundOrder);
 router.get('/admin',getAllOrders);
 router.get("/admin/stats", getAdminOrderStats);
+
+//Report Generation
+router.get('/admin/report', generateOrderReport);
+
 router.get('/admin/:orderId', getOrderDetails);
 router.patch('/admin/:orderId/status', updateOrderStatus);
 
